81e0883f5bac7101b3455f0774974f93d23f0b64
Abinet <agonaferdegefe@gmail.com>
2025-09-14 13:15:07 +0300

    learning how the backend recieves data from the frontned and how the frontend will recienve dynamic values from backend

--- core/views.py
+++ core/views.py
@@ -3,5 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
 def about(request):
-    return HttpResponse('<h1>About Page</h1><p>This is the About Page.</p>')
+    return render(request, 'about.html')   
+
+def counter(request):
+    text = request.GET['text']
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html', {'amount': amount_of_words}) 
