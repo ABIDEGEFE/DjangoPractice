09b1d34c34913e880853e0a13eed8f15e829e959
Abinet <agonaferdegefe@gmail.com>
2025-09-15 19:02:43 +0300

    learning about how Django knows static files and how Django manages CSRF attack

--- core/views.py
+++ core/views.py
@@ -8,6 +8,6 @@ def about(request):
     return render(request, 'about.html')   
 
 def counter(request):
-    text = request.GET['text']
+    text = request.POST['text']
     amount_of_words = len(text.split())
     return render(request, 'counter.html', {'amount': amount_of_words}) 
